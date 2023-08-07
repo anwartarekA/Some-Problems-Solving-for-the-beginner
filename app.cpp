36b2903e982d020e3cb7e1788609ccb9f0db27df
anwartarekA <anwartarek719@gmail.com>
2023-08-08 01:33:54 +0300

    Problems Solving

---
+++ app.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+using namespace std;
+int main(){
+ // program to calculate the average of the 6 grades
+
+    // int counter = 1 , degree , sum = 0;
+    // while(counter <= 6)
+    // {
+    //     cout << "Enter your degree \n";
+    //     cin >> degree;
+    //     sum += degree;
+    //     counter ++;
+    // }
+    // cout << "The average is = " << sum / 6<< "\n"; 
+// _________________________________________________________________________
+
+  // program to calculate the sum of the positive number
+    //   int number = 0,sum = 0;
+    // while(number >= 0)
+    // {     
+    //     cout << "Enter the number \n";
+    //     cin >> number;
+    //     sum += number;
+    // }
+    // cout << "The sum = " << sum << "\n";
+// _________________________________________________________________________
+
+// program to claculate the average of 5 degree and exit while enter -1 
+
+// int counter = 1, grade , sum = 0;
+//     while(counter <=5 && grade != -1)
+//     {
+//         cout << "Enter the grade \n";
+//         cin >> grade;
+//         sum += grade;
+//         counter ++ ;
+//     }
+//     cout << "the average is = " << sum / 5 << endl; 
+
+// program to print the numbers form 0 to 10
+
+// for (int i =0 ; i<= 10; i++)
+// {
+//     cout << i << "\n";
+// }
+
+// for (int i = 10; i >= 0; i--)
+// {
+//     cout << i << endl;
+// }
+
+//int x;
+    // for(x =2; x<=20 ; x+=2) cout << x << endl;
+
+// program that calculate the sum of even numbers and su, of odd numbers form the users
+
+    // int number, sumeven = 0,sumodd = 0;
+    // for(int i = 1 ; i<= 10; i++)
+    // {   
+    //     cout << "Enter the number \n";
+    //     cin >> number;
+    //     if(number % 2 == 0)
+    //     {
+    //             sumeven += number;
+    //     }
+    //     else
+    //     {
+    //         sumodd += number;
+    //     }
+    // }
+    // cout <<" the sum of even numbers is = "<< sumeven << endl;
+    // cout <<" the sum of odd numbers is = "<< sumodd << endl;
+
+    // program that calculate the factorial numver from the user
+
+    //   int number, factorial=1 ; 
+    // cout << "Enter the positive number \n";
+    // cin >> number;
+    // for(int i = 1; i<=number; i++)
+    // {
+    //     factorial *= i;
+    // }
+    // cout << "The factorial number = " << factorial << endl;
+
+// program that calculate the factorial number between 1 to 10 
+
+//     int counter = 1 , factorial_num = 1;
+//   while(counter <= 10)
+//   {
+//     factorial_num *= counter;
+//     counter++;
+//   }
+//   cout << "The factorial number between 1 to 10 = " <<factorial_num<<endl;
+
+// progrsm that print * 
+
+
+    // for(int i = 1; i<=5; i++)
+    // {
+    //     for(int j = i; j>=1; j--)
+    //     {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+    //     for(int i =1; i<=12; i++)
+    // {   
+    //      cout <<"The table multiplications of " << i << "\n";
+    //     for(int j=i; j<=12; j++ )
+    //     {
+    //         cout << i << " * " <<j<<" = " << i*j << " "<<"\n";
+    //     }
+    //     cout << '\n';
+    // }
+}
